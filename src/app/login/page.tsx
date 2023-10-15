322ca8e6e1cd3ab5486e941559312c66915ba64f
MohammadNasir520 <nasirahsan520@gmail.com>
2023-10-15 13:49:32 +0600

    from provider create done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,36 +1,5 @@
-import { type } from "os";
-import React, { ReactElement, ReactNode } from "react";
-import {
-  useForm,
-  FormProvider,
-  useFormContext,
-  SubmitHandler,
-} from "react-hook-form";
-
-type FormConfig = {
-  defaultValues?: Record<string, any>;
+const Login = () => {
+  return <div>login</div>;
 };
 
-type FormProps = {
-  submitHandler: SubmitHandler<any>;
-  children: ReactNode | ReactElement;
-} & FormConfig;
-
-const Form = () => {
-  const methods = useForm();
-  const formConfig: FormConfig = {};
-
-  const onSubmit = ({
-    children,
-    submitHandler,
-    defaultValues,
-  }: FormProps) => {};
-
-  return (
-    <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onSubmit)}></form>
-    </FormProvider>
-  );
-};
-
-export default Form;
+export default Login;
---
+++ src/components/Form/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { type } from "os";
+import React, { ReactElement, ReactNode } from "react";
+import { useForm, FormProvider, SubmitHandler } from "react-hook-form";
+
+type FormConfig = {
+  defaultValues?: Record<string, any>;
+};
+
+type FormProps = {
+  submitHandler: SubmitHandler<any>;
+  children?: ReactNode | ReactElement;
+} & FormConfig;
+
+const Form = ({ children, defaultValues, submitHandler }: FormProps) => {
+  const formConfig: FormConfig = {};
+
+  if (!!defaultValues) {
+    formConfig.defaultValues = defaultValues;
+  }
+  const methods = useForm<FormProps>(formConfig);
+  const { handleSubmit, reset } = methods;
+
+  console.log("methods", methods);
+
+  const onSubmit = (data: any) => {
+    handleSubmit(data);
+    reset();
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)}> {children}</form>
+    </FormProvider>
+  );
+};
+
+export default Form;
