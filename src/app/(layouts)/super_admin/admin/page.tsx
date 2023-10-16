95a7dc994a3626413eaf0761d96c084c37c780a0
MohammadNasir520 <nasirahsan520@gmail.com>
2023-10-16 21:33:50 +0600

    admin data fetched and showed in table resuably

--- src/app/(layouts)/super_admin/admin/page.tsx
+++ src/app/(layouts)/super_admin/admin/page.tsx
@@ -2,12 +2,66 @@
 
 import Table from "@/components/ui/Table/Table";
 import { useGetAllAdminQuery } from "@/redux/api/adminApi/adminApi";
+import { Button } from "antd";
+
+interface DataType {
+  key: React.Key;
+  name: string;
+  email: string;
+  contactNo: number;
+  profileImg: string;
+}
 
 const Admin = () => {
   const { data } = useGetAllAdminQuery(undefined);
-  console.log("admins", data);
+  console.log("admins", data.data);
+  const admins = data?.data;
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+    },
+    {
+      title: "Contact No",
+      dataIndex: "contactNo",
+    },
+    {
+      title: "Action",
+      render: function (data: any) {
+        return <Button onClick={() => console.log(data)}>edit</Button>;
+      },
+    },
+  ];
 
-  return <Table></Table>;
+  const tableData: DataType[] = [
+    {
+      key: "1",
+      name: "John",
+      email: "Brown",
+      contactNo: 32,
+      profileImg: "New York No. 1 Lake Park",
+    },
+    {
+      key: "2",
+      name: "John",
+      email: "Brown",
+      contactNo: 32,
+      profileImg: "New York No. 1 Lake Park",
+    },
+    {
+      key: "3",
+      name: "John",
+      email: "Brown",
+      contactNo: 32,
+      profileImg: "New York No. 1 Lake Park",
+    },
+  ];
+
+  return <Table columns={columns} dataSource={admins}></Table>;
 };
 
 export default Admin;
--- src/components/ui/Table/Table.tsx
+++ src/components/ui/Table/Table.tsx
@@ -1,40 +1,15 @@
 "use client";
 import React from "react";
-import { Space, Table as AntdTable, Tag } from "antd";
+import { Table as AntdTable } from "antd";
 
-interface DataType {
-  key: React.Key;
-  name: string;
-  email: string;
-  contactNo: number;
-  profileImg: string;
-}
+type TableProps = {
+  loading?: boolean;
+  columns: any;
+  dataSource: any;
+};
 
-const columns = [
-  {
-    title: "Name",
-    dataIndex: "name",
-  },
-  {
-    title: "Email",
-    dataIndex: "email",
-  },
-  {
-    title: "Contact No",
-    dataIndex: "contactNo",
-  },
-];
-
-const data: DataType[] = [
-  {
-    key: "1",
-    name: "John",
-    email: "Brown",
-    contactNo: 32,
-    profileImg: "New York No. 1 Lake Park",
-  },
-];
-
-const Table = () => <AntdTable dataSource={data} columns={columns}></AntdTable>;
+const Table = ({ columns, dataSource }: TableProps) => {
+  return <AntdTable dataSource={dataSource} columns={columns}></AntdTable>;
+};
 
 export default Table;
