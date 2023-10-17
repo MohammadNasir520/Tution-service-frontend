5c69a84fb226a65b9f9510ad4317fddbf6b245d6
MohammadNasir520 <nasirahsan520@gmail.com>
2023-10-17 21:26:15 +0600

    gettitng error in redux error properly

--- src/helpres/axios/axios.ts
+++ src/helpres/axios/axios.ts
@@ -6,7 +6,7 @@ import type { AxiosRequestConfig, AxiosError } from 'axios'
 import { instance  as axiosInstance} from './axiosInstance'
 
 
-//amar code
+
 export const axiosBaseQuery =
   (
     { baseUrl }: { baseUrl: string } = { baseUrl: '' }
@@ -47,40 +47,6 @@ export const axiosBaseQuery =
       }
     }
   }
-
-//module code
-// export const axiosBaseQuery =
-//   (
-//     { baseUrl }: { baseUrl: string } = { baseUrl: '' }
-//   ): BaseQueryFn<
-//     {
-//       url: string
-//       method: AxiosRequestConfig['method']
-//       data?: AxiosRequestConfig['data']
-//           params?: AxiosRequestConfig['params']
-//       meta?: IMeta
-//       contentType?: string
-//     },
-//     unknown,
-//     unknown
-//   > =>
-//   async ({ url, method, data, params, contentType }) => {
-//     try {
-//       const result = await axiosInstance({
-//         url: baseUrl + url, method, data, params, headers: {
-//         contentType: contentType || "application/json"
-//       } })
-//       return { data: result.data }
-//     } catch (axiosError) {
-//       let err = axiosError as AxiosError
-//       return {
-//         error: {
-//           status: err.response?.status,
-//           data: err.response?.data || err.message,
-//         },
-//       }
-//     }
-//   }
 
 
 
