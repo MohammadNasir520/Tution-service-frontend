e69f6a2313623ffaf67dc143c377f41be6260b79
MohammadNasir520 <nasirahsan520@gmail.com>
2023-11-03 00:34:29 +0600

    news problem solved

--- src/components/ui/LatestUpdateCard/LatestUpdateCard.tsx
+++ src/components/ui/LatestUpdateCard/LatestUpdateCard.tsx
@@ -1,39 +1,33 @@
-import { getUserInfo } from "@/services/authServices";
 import Image from "next/image";
-import Link from "next/link";
-import { AiOutlineShoppingCart } from "react-icons/ai";
-import { TbCurrencyTaka } from "react-icons/tb";
 
-const LatestUpdateCard = ({ service }: any) => {
-  const description = "kaslkfas";
+const LatestUpdateCard = ({ latestUpdate }: any) => {
+  const { title, image, text } = latestUpdate;
+
   return (
-    <div className=" md:px-20">
-      <div className="relative flex flex-col    my-3    lg:min-h-[450px] w-full   rounded-lg bg-gray-100 border text-gray-700  ">
-        <div className="relative    overflow-hidden  bg-blue-gray-500 bg-clip-border text-white  ">
-          <div className="flex justify-center h-[300px]   ">
-            <Image
-              src={"/breakingNews.jpeg"}
-              width={300}
-              height={350}
-              alt="pic"
-              layout="responsive"
-            ></Image>
-          </div>
+    <div className="relative flex flex-col w-[340px] my-3    lg:min-h-[350px]  rounded-lg bg-gray-100 border text-gray-700  ">
+      <div className="relative    overflow-hidden  bg-blue-gray-500 bg-clip-border text-white  ">
+        <div className="flex justify-center h-[200px] ">
+          <Image
+            src={image}
+            width={300}
+            height={350}
+            alt="pic"
+            layout="responsive"
+          ></Image>
         </div>
-        <div className="">
-          <div className="my-2 flex flex-col ">
-            <h5 className="block hover:text-blue-500   text-xl font-bold leading-snug tracking-normal text-black antialiased">
-              Title
-            </h5>
-          </div>
-          <hr />
-          <div className="px-2 h-6 mb-2">
-            <h4>
-              {" "}
-              {description.slice(0, 50)}{" "}
-              {description.length > 50 ? ".... " : ""}
-            </h4>
-          </div>
+      </div>
+      <div className="">
+        <div className="my-2 px-1 flex flex-col ">
+          <h5 className="block hover:text-blue-500   text-lg font-bold leading-snug tracking-normal text-black antialiased">
+            {title}
+          </h5>
+        </div>
+        <hr />
+        <div className="px-2 min-h-6 mb-2">
+          <h4>
+            {" "}
+            {text.slice(0, 80)} {text.length > 80 ? ".... " : ""}
+          </h4>
         </div>
       </div>
     </div>
--- src/components/view/LatestUpdate/LatestUpdate.tsx
+++ src/components/view/LatestUpdate/LatestUpdate.tsx
@@ -1,14 +1,27 @@
+"use client";
 import LatestUpdateCard from "@/components/ui/LatestUpdateCard/LatestUpdateCard";
+import { useGetAlllatestUpdateQuery } from "@/redux/api/latestUpdateApi/latestUpdateApi";
 import React from "react";
 
 const LatestUpdate = () => {
+  const { data: latestUpdates } = useGetAlllatestUpdateQuery(undefined);
+  console.log(latestUpdates);
+
   return (
-    <div>
+    <div className="my-12">
       <h1 className="text-xl font-bold text-center">Our Latest Update</h1>
-      <div className="grid md:grid-cols-2 ">
-        <LatestUpdateCard></LatestUpdateCard>
-        <LatestUpdateCard></LatestUpdateCard>
-        <LatestUpdateCard></LatestUpdateCard>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 justify-center items-center">
+        {latestUpdates?.map((latestUpdate: any) => {
+          return (
+            <div
+              key={latestUpdate?.id}
+              className="flex justify-center items-center w-full"
+            >
+              <LatestUpdateCard latestUpdate={latestUpdate}></LatestUpdateCard>;
+            </div>
+          );
+        })}
       </div>
     </div>
   );
